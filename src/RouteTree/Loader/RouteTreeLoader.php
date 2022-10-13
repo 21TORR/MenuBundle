aa4e6f08588cf0b782cb504366974a93aa3630d1
Jannik Zschiesche <j.zschiesche@21torr.com>
2022-10-13 14:09:33 +0200

    Add cache functionality for route tree loader

--- src/RouteTree/Loader/RouteTreeLoader.php
+++ src/RouteTree/Loader/RouteTreeLoader.php
@@ -2,14 +2,20 @@
 
 namespace Torr\MenuBundle\RouteTree\Loader;
 
+use Symfony\Component\Config\ConfigCacheFactoryInterface;
+use Symfony\Component\Config\ConfigCacheInterface;
 use Symfony\Component\Routing\RouterInterface;
 use Torr\MenuBundle\Exception\MissingDependencyException;
 use Torr\MenuBundle\RouteTree\Collection\RouteTreeCollection;
 
 final class RouteTreeLoader
 {
+	/**
+	 */
 	public function __construct (
 		private readonly RouteTreeCollectionBuilder $builder,
+		private readonly ConfigCacheFactoryInterface $configCacheFactory,
+		private readonly string $cacheDir,
 		private readonly ?RouterInterface $router = null,
 	) {}
 
@@ -23,7 +29,23 @@ final class RouteTreeLoader
 			throw new MissingDependencyException("Can't use the route tree without a router.");
 		}
 
-		return $this->builder->build($this->router->getRouteCollection());
-	}
+		$cache = $this->configCacheFactory->cache(
+			"{$this->cacheDir}/21torr/menu-bundle/route-tree-collection.php",
+			function (ConfigCacheInterface $cache) : void
+			{
+				$routeCollection = $this->router->getRouteCollection();
+				$routeTreeCollection = $this->builder->build($routeCollection);
 
+				$cache->write(
+					\sprintf(
+						'<?php return \\unserialize(%s);',
+						\var_export(\serialize($routeTreeCollection), true),
+					),
+					$routeCollection->getResources(),
+				);
+			},
+		);
+
+		return include $cache->getPath();
+	}
 }
